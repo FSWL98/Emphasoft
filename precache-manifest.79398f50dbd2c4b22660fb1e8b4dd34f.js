f94cd59b2d3df4233093d31989b218980b0df34a
FSWL <fswl98@gmail.com>
2020-06-09 19:15:03 +0300

    Updates

---
+++ precache-manifest.79398f50dbd2c4b22660fb1e8b4dd34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f3da11f08b6f7fc3b3f56cf8826a63",
+    "url": "/Emphasoft/index.html"
+  },
+  {
+    "revision": "85a5b140c38a3f7a1e2d",
+    "url": "/Emphasoft/static/css/main.5f8157fa.chunk.css"
+  },
+  {
+    "revision": "de7e5231c16311b89e2b",
+    "url": "/Emphasoft/static/js/2.13ddd860.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Emphasoft/static/js/2.13ddd860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a5b140c38a3f7a1e2d",
+    "url": "/Emphasoft/static/js/main.909167f9.chunk.js"
+  },
+  {
+    "revision": "3c3bd97362c2d9fd2fe9",
+    "url": "/Emphasoft/static/js/runtime-main.5e1d931d.js"
+  }
+]);
